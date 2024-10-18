0b0a8e0938ffce1cc71cfb996f5ce7ea1bb70ba4
Rud356 <r.moroshkin2015@gmail.com>
2024-10-18 16:56:04 +0300

    Implement logging out of account

---
+++ src/views/logout_from_account.py
@@ -0,0 +1,18 @@
+from aiohttp import web
+
+
+async def handle_logout(request: web.Request) -> web.Response:
+    """
+    Removes cookie on client side with authentication token if it's present.
+
+    :param request: http request.
+    :return: web response with header that removes cookie.
+    """
+
+    if request.cookies.get("UserToken"):
+        response: web.Response = web.Response()
+        response.del_cookie("UserToken")
+        return response
+
+    else:
+        return web.Response(body="Not authorized".encode("utf8"), status=401)
